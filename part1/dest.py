1117858d96548b283392c294b01ca4eb640fd94c
Onur <onurcantirtir@gmail.com>
2018-11-21 04:46:59 +0300

    Some renamings and discussions are made xd

--- part1/dest.py
+++ part1/dest.py
@@ -9,7 +9,8 @@ import random
 HEADER_SIZE = 4
 ACK_MESSAGE = "1" 
 NACK_MESSAGE = "0"
-MAX_CONNECTIONS = 5
+MAX_CONNECTIONS = 1
+BIND_IP = "10.10.3.2"
 
 def get_int_from_binary(byte):
     return int(byte.decode('utf-8'))
@@ -17,27 +18,27 @@ def get_int_from_binary(byte):
 def get_binary_from_string(string):
     return bytearray(string, 'utf-8')
 
-def worker_clientsocket(clientsocket, clientid):
+def worker_connectionsocket(connectionsocket, clientid):
     # get max size of a file
-    data = clientsocket.recv(HEADER_SIZE)
+    data = connectionsocket.recv(HEADER_SIZE)
     MAX_FILE_SIZE = get_int_from_binary(data)
     # send ack
-    clientsocket.send(get_binary_from_string(ACK_MESSAGE))
+    connectionsocket.send(get_binary_from_string(ACK_MESSAGE))
 
     while True:
         # get each file / chunk
 
-        data = clientsocket.recv(MAX_FILE_SIZE)
+        data = connectionsocket.recv(MAX_FILE_SIZE)
         print("Get data from", clientid)
         time.sleep(random.uniform(0.2, 1.0))
-        clientsocket.send(get_binary_from_string(ACK_MESSAGE))
+        connectionsocket.send(get_binary_from_string(ACK_MESSAGE))
         print("Sent data to", clientid)
 
 def main():
     port = int(sys.argv[1])
 
     serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    serversocket.bind(('localhost', port))
+    serversocket.bind((BIND_IP, port))
     serversocket.listen(MAX_CONNECTIONS)
     
     nconnections = 0
@@ -45,11 +46,11 @@ def main():
     while nconnections < MAX_CONNECTIONS:
         nconnections += 1
         
-        clientsocket, address = serversocket.accept()
+        connectionsocket, address = serversocket.accept()
         print("Welcome Client nconnections", nconnections)
 
-        thread = threading.Thread(target=worker_clientsocket, 
-                args=(clientsocket, nconnections,))
+        thread = threading.Thread(target=worker_connectionsocket, 
+                args=(connectionsocket, nconnections,))
         thread.start()
 
     serversocket.close()
--- part1/source.py
+++ part1/source.py
@@ -8,6 +8,7 @@ ACK_MESSAGE = "1"
 NACK_MESSAGE = "0"
 MAX_FILE_SIZE = 1000
 STRING = "hello world xd xd 122458?**hello world xd xd 122458?**hello world xd xd 122458?**"
+DEST_IP = "10.10.3.2"
 
 def get_binary_from_int(integer):
     return bytearray(str(integer), 'utf-8')
