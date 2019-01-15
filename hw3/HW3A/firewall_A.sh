376554347e98af02aa092cd52fbcba588c5ef5f4
Onur TIRTIR <onurcantirtir@gmail.com>
2019-01-16 01:55:51 +0300

    See checklist

--- hw3/HW3A/firewall_A.sh
+++ hw3/HW3A/firewall_A.sh
@@ -1,27 +1,31 @@
 #!/usr/bin/env bash
 
+HOSTB=192.168.43.70
+
 # disable firewall
 if [[ $1 == "-d" ]];
 then
-    ufw reset
-
     ufw disable
+    ufw reset
+    ufw disable
+
     exit
 fi
 
 # enable firewall
 if [[ $1 == "-e" ]];
 then
+    ufw reset
     ufw enable
-    
-    # facebook
-    ufw reject from 157.240.9.35 to any
-    # telnet
-    ufw reject from 192.168.43.5 to any port 23
-    # icmp
-    ufw reject 7
-    # https
-    ufw reject https
+
+    # for icmp packets
+    cp etc.ufw.before.rules /etc/ufw/before.rules
+    # for facebook
+    ufw deny out from any to 157.240.9.35
+    # for telnet
+    ufw deny proto tcp from ${HOSTB} to any
+
+    ufw reload
 
     exit
 fi
