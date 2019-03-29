006f8c62f8a12c430a4f3f606d6bc5116a731d8c
Onur TIRTIR <onurcantirtir@gmail.com>
2019-03-30 02:22:42 +0300

    Update rasterizer.cpp

--- ceng477 - Computer Graphics/hw2/source/rasterizer.cpp
+++ ceng477 - Computer Graphics/hw2/source/rasterizer.cpp
@@ -52,275 +52,6 @@ Vec3 getVec3FromVec4(const Vec4 & vec) {
 
     return r;
 }
-/*
-class Vec4 {
-public:
-    Vec4(double x=0, double y=0, double z=0, double w=0, int colorId=0);
-    // pad 1 to last dimension
-    explicit Vec4(const Vec3 & rhs);
-    void make_homogenous();
-    double x, y, z, w;
-    int colorId;
-};
-
-typedef struct Matrix_4_4 {
-    double data[4][4];
-
-    Matrix_4_4();
-    Matrix_4_4(const Matrix_4_4 & rhs);
-
-    // XXX One of them should be called just after constructor and ONLY
-    // XXX one of them should be used for a Matrix_4_4
-    void makeIdentity();
-    void makeTranslation(const Translation & t);
-    void makeTranslation(double x, double y, double z);
-    void makeScale(const Scaling & s);
-    void makeRotationArbitrary(const Rotation & r);
-    void makeFrom3Vec3(const Vec3 & u, const Vec3 & v, const Vec3 & w);
-
-    Matrix_4_4 multiplyBy(const Matrix_4_4 & rhs) const;
-    Vec4 multiplyBy(const Vec4 & rhs) const;
-    Vec3 multiplyBy(const Vec3 & rhs) const;
-    Matrix_4_4 getTranspose() const;
-
-private:
-    // for rotation, helpers, not for explicit use
-    void makeM(const Rotation & r);
-    void makeRotationAroundX(const Rotation & r);
-
-} Matrix_4_4;
-
-class LineEquation {
-private:
-    double y0_y1;
-    double x1_x0;
-    double x0y1_y0x1;
-public:
-    LineEquation(const Vec4 & v0, const Vec4 & v1);
-    double operator()(double x, double y);
-};
-
-void Matrix_4_4::makeIdentity() {
-    data[0][0] = 1;
-    data[1][1] = 1;
-    data[2][2] = 1;
-    data[3][3] = 1;
-}
-
-Matrix_4_4 Matrix_4_4::multiplyBy(const Matrix_4_4 & rhs) const {
-    double total;
-
-    Matrix_4_4 result;
-
-    for (int i = 0; i < 4; i++) {
-        for (int j = 0; j < 4; j++) {
-            total = 0;
-            for (int k = 0; k < 4; k++) {
-                total += data[i][k] * rhs.data[k][j];
-            }
-            result.data[i][j] = total;
-        }
-    }
-
-    return result;
-}
-
-void Matrix_4_4::makeTranslation(const Translation & t) {
-    makeIdentity();
-
-    data[0][3] = t.tx;
-    data[1][3] = t.ty;
-    data[2][3] = t.tz;
-}
-
-void Matrix_4_4::makeTranslation(double x, double y, double z) {
-    makeIdentity();
-
-    data[0][3] = x;
-    data[1][3] = y;
-    data[2][3] = z;
-}
-
-void Matrix_4_4::makeScale(const Scaling & s) {
-    makeIdentity();
-
-    data[0][0] = s.sx;
-    data[1][1] = s.sy;
-    data[2][2] = s.sz;
-}
-
-// internal method for rotation
-Vec3 __calculate_v(const Vec3 & u) {
-
-    Vec3 v;
-
-    std::vector<double> u_vector({u.x, u.y, u.z});
-    int i = std::distance(std::begin(u_vector), std::min_element(std::begin(u_vector), std::end(u_vector)));
-
-    if(i == 0){
-        v.x = 0;
-        v.y = -u.z;
-        v.z = u.y;
-    }
-    else if (i == 1) {
-        v.y = 0;
-        v.x = -u.z;
-        v.z = u.x;
-    }
-    else { //(i == 2)
-        v.z = 0;
-        v.x = -u.y;
-        v.y = u.x;
-    }
-
-    return v;
-}
-
-
-
-void Matrix_4_4::makeM(const Rotation & r) {
-    Vec3 u, v, w;
-
-    u.x = r.ux;
-    u.y= r.uy;
-    u.z = r.uz;
-
-    u = normalizeVec3(u);
-
-    data[0][0] = u.x;
-    data[0][1] = u.y;
-    data[0][2] = u.z;
-
-    v = __calculate_v(u);
-    v = normalizeVec3(v);
-    data[1][0] = v.x;
-    data[1][1] = v.y;
-    data[1][2] = v.z;
-
-    w = crossProductVec3(u, v);
-    w = normalizeVec3(w);
-
-    data[2][0] = w.x;
-    data[2][1] = w.y;
-    data[2][2] = w.z;
-
-    data[3][3] = 1;
-}
-
-Matrix_4_4 Matrix_4_4::getTranspose() const {
-    Matrix_4_4 result;
-
-    for (int i = 0; i < 4; i++) {
-        for (int j = 0; j < 4; j++) {
-            result.data[i][j] = data[j][i];
-        }
-    }
-
-    return result;
-}
-
-void Matrix_4_4::makeRotationAroundX(const Rotation & r) {
-    double angle_radian = r.angle * M_PI / 180;
-
-    double cosine = cos(angle_radian);
-    double sine = sin(angle_radian);
-
-    data[0][0] = 1;
-
-    data[1][1] = cosine;
-    data[1][2] = -sine;
-
-    data[2][1] = sine;
-    data[2][2] = cosine;
-
-    data[3][3] = 1;
-}
-
-// rotate "angle" degrees around arbitrary axis
-void Matrix_4_4::makeRotationArbitrary(const Rotation & r) {
-    Matrix_4_4 T;
-//    T.makeTranslation(-r.ux, -r.uy, -r.uz);
-    Matrix_4_4 T_inv;
-//    T_inv.makeTranslation(r.ux, r.uy, r.uz);
-
-    Matrix_4_4 M;
-    M.makeM(r);
-
-    Matrix_4_4 M_inv = M.getTranspose();
-
-    Matrix_4_4 rotX;
-    rotX.makeRotationAroundX(r);
-
-    Matrix_4_4 result = M_inv.multiplyBy(rotX.multiplyBy(M));
-
-    memcpy(data, result.data, 16 * sizeof(double));
-}
-
-void Matrix_4_4::makeFrom3Vec3(const Vec3 & u, const Vec3 & v, const Vec3 & w) {
-    data[0][0] = u.x;
-    data[0][1] = u.y;
-    data[0][2] = u.z;
-
-    data[1][0] = v.x;
-    data[1][1] = v.y;
-    data[1][2] = v.z;
-
-    data[2][0] = w.x;
-    data[2][1] = w.y;
-    data[2][2] = w.z;
-
-    data[3][3] = 1;
-}
-
-Vec4 Matrix_4_4::multiplyBy(const Vec4 & rhs) const {
-    Vec4 result;
-
-    result.x = data[0][0] * rhs.x + data[0][1] * rhs.y + data[0][2] * rhs.z + data[0][3] * rhs.w;
-    result.y = data[1][0] * rhs.x + data[1][1] * rhs.y + data[1][2] * rhs.z + data[1][3] * rhs.w;
-    result.z = data[2][0] * rhs.x + data[2][1] * rhs.y + data[2][2] * rhs.z + data[2][3] * rhs.w;
-    result.w = data[3][0] * rhs.x + data[3][1] * rhs.y + data[3][2] * rhs.z + data[3][3] * rhs.w;
-
-    result.colorId = rhs.colorId;
-
-    return result;
-}
-
-Matrix_4_4::Matrix_4_4() {
-    memset(data, 0, sizeof(double) * 16);
-}
-
-Matrix_4_4::Matrix_4_4(const Matrix_4_4 & rhs) {
-    memcpy(data, rhs.data, 16 * sizeof(double));
-}
-
-Vec4::Vec4(const Vec3 & rhs) {
-    x = rhs.x;
-    y = rhs.y;
-    z = rhs.z;
-    w = 1;
-
-    colorId = rhs.colorId;
-}
-
-void Vec4::make_homogenous() {
-    x /= w;
-    y /= w;
-    z /= w;
-    w = 1;
-}
-
-Vec4::Vec4(double x, double y, double z, double w, int colorId) : x(x), y(y), z(z), w(w), colorId(colorId) {
-}
-
-LineEquation::LineEquation(const Vec4 & v0, const Vec4 & v1) {
-    y0_y1 = v0.y - v1.y;
-    x1_x0 = v1.x - v0.x;
-    x0y1_y0x1 = v0.x * v1.y - v0.y * v1.x;
-}
-
-double LineEquation::operator()(double x, double y) {
-    return x * y0_y1 + y * x1_x0 + x0y1_y0x1;
-}*/
 
 void debug_print(const std::string & error_message) {
     if (true == __DEBUG__) {
@@ -389,15 +120,10 @@ void apply_M_model(Model &model, std::map<int, std::vector<Vec4> *> *modelTriang
         for(Vec4 &vect : *triangleVertexPair.second){
             vect = M_model.multiplyBy(vect);
         }
-    }/*
-    for (std::pair <int, std::vector<Vec4> *> triangleVertexPair : *modelTriangleMap){
-        for(int i = 0; i < 3; i++){
-            triangleVertexPair.second[i] = M_model.multiplyBy(triangleVertexPair.second->at(i));
-        }
-    }*/
+    }
 }
 
-// bugfix XXX
+
 Matrix_4_4 calculate_M_Cam(const Camera & cam) {
     Vec3 corrected_u = crossProductVec3(cam.gaze, cam.v);
     Vec3 corrected_v = crossProductVec3(corrected_u, cam.gaze);
@@ -570,9 +296,6 @@ void midpointY(Vec4 v0, Vec4 v1, bool sign){
 	Using types in "hw2_types.h" and functions in "hw2_math_ops.cpp" will speed you up while working.
 */
 void forwardRenderingPipeline(Camera & cam, std::map<int, std::map<int, std::vector<Vec4> *> > *modelMap) {
-    // TODO: IMPLEMENT HERE
-
-    // TODO: make all the translating, scaling and rotating on models before viewport transformations
 
     // Calculate camera transformations - M_cam
     Matrix_4_4 M_cam = calculate_M_Cam(cam);
@@ -589,16 +312,11 @@ void forwardRenderingPipeline(Camera & cam, std::map<int, std::map<int, std::vec
         const Model & model = models[m];
 
         for (int t = 0; t < model.numberOfTriangles; t++) {
-            //std::vector<Vec3> &triangleVertexVector = *(modelMap->at(m).at(t));
 
             // Apply first part of matrix transformations
             Vec4 v0 = M_cam.multiplyBy(modelMap->at(m).at(t)->at(0));
             Vec4 v1 = M_cam.multiplyBy(modelMap->at(m).at(t)->at(1));
             Vec4 v2 = M_cam.multiplyBy(modelMap->at(m).at(t)->at(2));
-
-//            v0.make_homogenous();
-//            v1.make_homogenous();
-//            v2.make_homogenous();
 
             Vec3 v0_3 = getVec3FromVec4(v0);
             Vec3 v1_3 = getVec3FromVec4(v1);
@@ -608,14 +326,10 @@ void forwardRenderingPipeline(Camera & cam, std::map<int, std::map<int, std::vec
             normal = normalizeVec3(normal);
             double dot = dotProductVec3(normalizeVec3(v2_3), normal);
 
-            // TODO care precision
-
             bool backface_passed = (backfaceCullingSetting == 1 && dot < 0) || backfaceCullingSetting == 0;
-            //bool backface_passed = !(dot > 0 && backfaceCullingSetting != 0);
 
             // Apply viewport transformation and rasterization
             if (true == backface_passed) {
-                // bugfix XXX
 
                 // Apply first part of matrix transformations
                 v0 = M_per.multiplyBy(v0);
@@ -749,9 +463,6 @@ void forwardRenderingPipeline(Camera & cam, std::map<int, std::map<int, std::vec
                         xmax_int = cam.sizeX - 1;
                     }
 
-                    //if(xmin < 0 || xmax > cam.sizeX || ymin < 0 || ymax > cam.sizeY) continue;
-
-                    // TODO possible problem with pixel coordinate to image array
                     for (int y = ymin_int; y <= ymax_int; y++) {
                         for (int x = xmin_int; x <= xmax_int; x++) {
 
@@ -778,7 +489,6 @@ void forwardRenderingPipeline(Camera & cam, std::map<int, std::map<int, std::vec
     }
 }
 
-// TODO check application wise precision things
 
 int main(int argc, char **argv) {
 
@@ -821,28 +531,6 @@ int main(int argc, char **argv) {
 
         // initialize image with basic values
         initializeImage(cameras[c]);
-
-        // copy vertices to v_vertices as they are global and they will be modified separately for each camera
-        //std::vector<Vec4> v_vertices;
-        //TODO v_vertices.reserve(numberOfVertices);
-
-        // dummy
-        //v_vertices.push_back(Vec4());
-
-        //for (int v = 1; v <= numberOfVertices; v++) {
-            //v_vertices.push_back(Vec4(vertices[v]));
-        //}
-
-        /*
-        for(std::pair <int, std::map<int, std::vector<Vec4> *> > modelTrianglePair : modelMap){
-            for (std::pair <int, std::vector<Vec4> *> triangleVertexPair : modelTrianglePair.second){
-                for(Vec4 vect : *triangleVertexPair.second){
-                    std::cout << "Vertex coords:(" << vect.x << "," << vect.y << "," << vect.z << ")" << std::endl;
-                }
-                std::cout << "-------" << std::endl;
-            }
-        }
-        */
 
         /* do forward rendering pipeline operations */
         forwardRenderingPipeline(cameras[c], &modelMap);
